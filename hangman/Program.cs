0a17b04091705f6bca613d219ac1e0a2e03fddca
Ju111 <SchneiderJu@gmx.net>
2018-08-05 22:23:35 +0200

    only accept characters between a and z as valid input

--- hangman/Program.cs
+++ hangman/Program.cs
@@ -20,30 +20,41 @@ namespace Hangman
         private string hello;
         private string input;
 
+        private char inputChar;
+
         private List<string> words = new List<string>();
         private int index;
         private string word;
 
-        internal List<string> guesses = new List<string>();
+        internal List<char> guesses = new List<char>();
 
         internal Hangman()
         {
-            words.Add("c#");
-            words.Add("java");
-            words.Add("css");
-            words.Add("javascript");
-            words.Add("html");
-            words.Add("typescript");
-            words.Add("php");
-            words.Add("ruby");
-            words.Add("sql");
-            words.Add("c");
-            words.Add("c++");
-            words.Add("go");
-            words.Add("python");
+            words.Add("leiden");
+            words.Add("delft");
+            words.Add("amsterdam");
+            words.Add("haarlem");
+            words.Add("utrecht");
+            words.Add("groningen");
+            words.Add("rotterdam");
+            words.Add("arnhem");
+            words.Add("dokkum");
+            words.Add("leeuwarden");
+            words.Add("appeldoorn");
+            words.Add("nijmegen");
+            words.Add("maastricht");
+            words.Add("venlo");
+            words.Add("eindhoven");
+            words.Add("breda");
+            words.Add("oss");
+            words.Add("tilburg");
+            words.Add("enschede");
+            words.Add("zwolle");
+            words.Add("gouda");
+            words.Add("amersfoort");
         }
 
-        internal void addToList(string userInput)
+        internal void addToList(char userInput)
         {
             guesses.Add(userInput);
         }
@@ -52,7 +63,7 @@ namespace Hangman
         {
             if (guesses.Count == 0)
             {
-                hello = "Today I will let you guess programming languages. Ready to go? Then pelease guess your first letter and press enter.";
+                hello = "Today I will let you gess Dutch cities. Ready to go? Then pelease guess your first letter and press enter.";
                 Console.ForegroundColor = ConsoleColor.DarkCyan;
                 Console.WriteLine(hello);
             }
@@ -65,12 +76,19 @@ namespace Hangman
 
             Console.ForegroundColor = ConsoleColor.Cyan;
             input = Console.ReadLine().ToLower();
+            inputChar = char.Parse(input);
 
-            Console.ForegroundColor = ConsoleColor.DarkCyan;
-            Console.WriteLine("Player wrote: " + input);
-            addToList(input);
-            Console.WriteLine(guesses.Count);
-
+            if (inputChar >= 'a' && inputChar <= 'z')
+            {
+                Console.ForegroundColor = ConsoleColor.DarkCyan;
+                Console.WriteLine("Player wrote: " + input);
+                addToList(inputChar);
+                Console.WriteLine(guesses.Count);
+            }
+            else
+            {
+                Console.WriteLine("Please choose a letter bewteen a and z!");
+            }
         }
     }
 }
